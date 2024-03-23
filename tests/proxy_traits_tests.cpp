45c01ce57108946eeed602c8f11571e92c2b7616
Mingxin Wang <mingxwa@microsoft.com>
2024-03-23 16:05:53 +0900

    Header missing

--- tests/proxy_traits_tests.cpp
+++ tests/proxy_traits_tests.cpp
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+#include <array>
 #include <stdexcept>
 #include <type_traits>
 #include "proxy.h"
