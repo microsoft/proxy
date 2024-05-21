2d781c0da2f0fdddf60d35abaeef89fc73fe57af
Mingxin Wang <mingxwa@microsoft.com>
2024-05-21 20:05:11 +0800

    Add inplace_proxiable_target

--- proxy.h
+++ proxy.h
@@ -856,14 +856,15 @@ proxy<F> make_proxy_inplace(T&& value)
 
 #if __STDC_HOSTED__
 template <class T, class F>
-concept proxiable_target =
-    proxiable<details::compact_ptr<T, std::allocator<T>>, F>;
+concept allocated_proxiable_target =
+    proxiable<details::compact_ptr<T, std::allocator<void>>, F>;
 
-template <class F, proxiable_target<F> T, class Alloc, class... Args>
+template <class F, allocated_proxiable_target<F> T, class Alloc, class... Args>
 proxy<F> allocate_proxy(const Alloc& alloc, Args&&... args) {
   return details::allocate_proxy_impl<F, T>(alloc, std::forward<Args>(args)...);
 }
-template <class F, proxiable_target<F> T, class Alloc, class U, class... Args>
+template <class F, allocated_proxiable_target<F> T, class Alloc, class U,
+    class... Args>
 proxy<F> allocate_proxy(const Alloc& alloc, std::initializer_list<U> il,
     Args&&... args) {
   return details::allocate_proxy_impl<F, T>(
@@ -874,6 +875,11 @@ proxy<F> allocate_proxy(const Alloc& alloc, T&& value) {
   return details::allocate_proxy_impl<F, std::decay_t<T>>(
       alloc, std::forward<T>(value));
 }
+
+template <class T, class F>
+concept proxiable_target = inplace_proxiable_target<T, F> ||
+    allocated_proxiable_target<T, F>;
+
 template <class F, proxiable_target<F> T, class... Args>
 proxy<F> make_proxy(Args&&... args)
     { return details::make_proxy_impl<F, T>(std::forward<Args>(args)...); }
