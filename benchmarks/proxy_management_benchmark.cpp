552feaeea45b203f515b41f0fd7e12621207864a
Mingxin Wang <mingxwa@microsoft.com>
2024-10-03 17:08:08 +0800

    Improve benchmark

--- benchmarks/proxy_management_benchmark.cpp
+++ benchmarks/proxy_management_benchmark.cpp
@@ -5,6 +5,7 @@
 #include <array>
 #include <deque>
 #include <forward_list>
+#include <string>
 #include <unordered_map>
 
 #include <benchmark/benchmark.h>
@@ -15,18 +16,35 @@ namespace {
 
 constexpr int TestManagedObjectCount = 12000;
 
+using TestSmallObjectType1 = int;
+using TestSmallObjectType2 = std::shared_ptr<int>;
+using TestSmallObjectType3 = std::forward_list<double>;
+
+using TestLargeObjectType1 = std::array<int, 32>;
+struct TestLargeObjectType2 {
+  int Field1;
+  std::vector<int> Field2;
+};
+struct TestLargeObjectType3 {
+  std::unordered_map<int, int> Field1;
+  std::deque<int> Field2;
+  std::string Field3;
+};
+
 }  // namespace
 
-struct AnyCopyable : pro::facade_builder::support_copy<pro::constraint_level::nontrivial>::build {};
+struct AnyCopyable : pro::facade_builder
+    ::support_copy<pro::constraint_level::nontrivial>  // Although it's not used, std::any supports copy
+    ::build {};
 
 void BM_SmallObjectManagementWithProxy(benchmark::State& state) {
   for (auto _ : state) {
     std::vector<pro::proxy<AnyCopyable>> data;
     data.reserve(TestManagedObjectCount);
     for (int i = 0; i < TestManagedObjectCount; i += 3) {
-      data.push_back(pro::make_proxy<AnyCopyable>(123));
-      data.push_back(pro::make_proxy<AnyCopyable, std::shared_ptr<int>>());
-      data.push_back(pro::make_proxy<AnyCopyable, std::forward_list<double>>());
+      data.push_back(pro::make_proxy<AnyCopyable, TestSmallObjectType1>());
+      data.push_back(pro::make_proxy<AnyCopyable, TestSmallObjectType2>());
+      data.push_back(pro::make_proxy<AnyCopyable, TestSmallObjectType3>());
     }
     benchmark::DoNotOptimize(data);
   }
@@ -37,9 +55,9 @@ void BM_SmallObjectManagementWithAny(benchmark::State& state) {
     std::vector<std::any> data;
     data.reserve(TestManagedObjectCount);
     for (int i = 0; i < TestManagedObjectCount; i += 3) {
-      data.emplace_back(123);
-      data.emplace_back(std::shared_ptr<int>{});
-      data.emplace_back(std::forward_list<double>{});
+      data.emplace_back(TestSmallObjectType1{});
+      data.emplace_back(TestSmallObjectType2{});
+      data.emplace_back(TestSmallObjectType3{});
     }
     benchmark::DoNotOptimize(data);
   }
@@ -50,9 +68,9 @@ void BM_LargeObjectManagementWithProxy(benchmark::State& state) {
     std::vector<pro::proxy<AnyCopyable>> data;
     data.reserve(TestManagedObjectCount);
     for (int i = 0; i < TestManagedObjectCount; i += 3) {
-      data.push_back(pro::make_proxy<AnyCopyable, std::array<int, 64>>());
-      data.push_back(pro::make_proxy<AnyCopyable, std::array<int, 64>>());
-      data.push_back(pro::make_proxy<AnyCopyable, std::array<int, 64>>());
+      data.push_back(pro::make_proxy<AnyCopyable, TestLargeObjectType1>());
+      data.push_back(pro::make_proxy<AnyCopyable, TestLargeObjectType2>());
+      data.push_back(pro::make_proxy<AnyCopyable, TestLargeObjectType3>());
     }
     benchmark::DoNotOptimize(data);
   }
@@ -63,9 +81,9 @@ void BM_LargeObjectManagementWithAny(benchmark::State& state) {
     std::vector<std::any> data;
     data.reserve(TestManagedObjectCount);
     for (int i = 0; i < TestManagedObjectCount; i += 3) {
-      data.emplace_back(std::array<int, 64>{});
-      data.emplace_back(std::array<int, 64>{});
-      data.emplace_back(std::array<int, 64>{});
+      data.emplace_back(TestLargeObjectType1{});
+      data.emplace_back(TestLargeObjectType2{});
+      data.emplace_back(TestLargeObjectType3{});
     }
     benchmark::DoNotOptimize(data);
   }
