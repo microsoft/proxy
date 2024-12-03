975535ee4ae0c8f39fde7296116be9de6c1070fb
Tian Liao <tilia@microsoft.com>
2024-12-03 19:48:00 +0800

    remove pack.sh

--- tools/pack.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-file="CMakeLists.txt"
-version=$(grep -oP 'msft_proxy\s+VERSION\s+\K[0-9]+\.[0-9]+\.[0-9]+' "$file")
-git tag "$version"
-git push origin "$version"
-tar -czf "proxy-$version.tgz" "proxy.h"
-echo "PRO_VER=$version" >> $GITHUB_OUTPUT
