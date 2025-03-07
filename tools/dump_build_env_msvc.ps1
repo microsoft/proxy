3901cb45bfd885bf7217548e9b1449792644582b
Mingxin Wang <mingxwa@microsoft.com>
2025-03-07 14:19:28 +0800

    Remove test code

--- tools/dump_build_env_msvc.ps1
+++ tools/dump_build_env_msvc.ps1
@@ -5,7 +5,6 @@ param(
 )
 
 $osInfo = Get-CimInstance Win32_OperatingSystem
-cl /? | Out-String
 $compiler = ((cl /? 2>&1 | Out-String) -split '\r')[0] -replace '^cl\s:\s',''
 $jsonObject = [ordered]@{
     OS = $osInfo.Caption
