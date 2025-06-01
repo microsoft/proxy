a97b5398fd733828bb7875b41eee4820d8ca37be
SidneyCogdill <no@localhost>
2025-06-01 00:18:04 +0000

    modify CMake scripts, part 1

--- cmake/proxyModuleTargets.cmake
+++ cmake/proxyModuleTargets.cmake
@@ -1,25 +1,25 @@
 
-if(NOT DEFINED proxy_INCLUDE_DIR)
-    message(FATAL_ERROR "proxy_INCLUDE_DIR must be defined to use this script.")
+if(NOT DEFINED proxy4_INCLUDE_DIR)
+    message(FATAL_ERROR "proxy4_INCLUDE_DIR must be defined to use this script.")
 endif()
 
-message(STATUS "Declaring `msft_proxy::module` target for include path ${proxy_INCLUDE_DIR}")
+message(STATUS "Declaring `msft_proxy::msft_proxy4_module` target for include path ${proxy4_INCLUDE_DIR}")
 
-add_library(msft_proxy_module)
+add_library(msft_proxy4_module)
 set_target_properties(
-  msft_proxy_module
+  msft_proxy4_module
   PROPERTIES
     SYSTEM TRUE
     EXCLUDE_FROM_ALL TRUE
 )
 
-add_library(msft_proxy::module ALIAS msft_proxy_module)
-target_sources(msft_proxy_module PUBLIC
+add_library(msft_proxy::msft_proxy4_module ALIAS msft_proxy4_module)
+target_sources(msft_proxy4_module PUBLIC
   FILE_SET CXX_MODULES
-  BASE_DIRS ${proxy_INCLUDE_DIR}
+  BASE_DIRS ${proxy4_INCLUDE_DIR}
   FILES
-    ${proxy_INCLUDE_DIR}/proxy/proxy.ixx
+    ${proxy4_INCLUDE_DIR}/proxy/proxy.ixx
 )
-target_compile_features(msft_proxy_module PUBLIC cxx_std_20)
-target_link_libraries(msft_proxy_module PUBLIC msft_proxy)
+target_compile_features(msft_proxy4_module PUBLIC cxx_std_20)
+target_link_libraries(msft_proxy4_module PUBLIC msft_proxy::msft_proxy4)
 
